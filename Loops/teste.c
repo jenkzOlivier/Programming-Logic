ee67c8f706f931319ec63f75d07834fcb27b391e
jenkzOlivier <143138841+jenkzOlivier@users.noreply.github.com>
2024-05-25 21:20:55 -0300

    updating loops
    update

--- Loops/teste.c
+++ Loops/teste.c
@@ -1,9 +1,13 @@
-#include<stdio.h>
-int main()
-{
-    for(int i = 0; i < 10; i++)
-    {
-        printf("Looping, ");
+#include <stdio.h>
+
+int main() {
+    char array[100]; // Array to store the name
+    printf("What's your name?: ");
+    scanf("%s", array); // No need for the & operator before array
+
+    for(int i = 0; i < 100; i++) {
+        printf("%s\n", array); // Print the name inside the loop
     }
+
     return 0;
 }
